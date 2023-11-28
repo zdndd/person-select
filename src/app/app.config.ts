d44db8bb7dc3ce3e9ee27f676bd24dbe65cd8e58
zdndd <dongningdella@gmail.com>
2023-11-28 21:03:08 +0800

    组件init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { zh_CN, provideNzI18n } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+registerLocaleData(zh);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideNzI18n(zh_CN),
+    importProvidersFrom(FormsModule),
+    importProvidersFrom(HttpClientModule),
+    provideAnimations(),
+  ],
 };
--- src/app/component/persion-select/persion-select.component.ts
+++ src/app/component/persion-select/persion-select.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReactiveFormsModule, ControlValueAccessor } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+
 @Component({
   selector: 'app-persion-select',
   standalone: true,
-  imports: [CommonModule],
+  imports: [NzInputModule, ReactiveFormsModule, CommonModule],
   templateUrl: './persion-select.component.html',
   styleUrl: './persion-select.component.less',
 })
-export class PersionSelectComponent {}
+export class PersionSelectComponent implements ControlValueAccessor {
+  writeValue(value: any): void {}
+  registerOnChange(fn: (_: any) => void): void {}
+  registerOnTouched(fn: any): void {}
+  setDisabledState(isDisabled: boolean): void {}
+}
