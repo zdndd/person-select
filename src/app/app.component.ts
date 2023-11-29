92203b6fd987b0420b1d1d6afbccc2d4312d092b
zdndd <dongningdella@gmail.com>
2023-11-29 17:39:04 +0800

    文字控件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import {
+  ReactiveFormsModule,
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+
 import { PersionSelectComponent } from './component/persion-select/persion-select.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PersionSelectComponent, CommonModule, RouterOutlet],
+  imports: [
+    PersionSelectComponent,
+    NzFormModule,
+    ReactiveFormsModule,
+    CommonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less',
 })
-export class AppComponent {}
+export class AppComponent {
+  validateForm: FormGroup<{
+    userName: FormControl<string>;
+    person: FormControl<string>;
+  }> = this.fb.group({
+    userName: ['della', [Validators.required]],
+    person: ['haha', [Validators.required]],
+  });
+
+  submitForm(): void {
+    console.log('submit', this.validateForm.value);
+  }
+
+  constructor(private fb: NonNullableFormBuilder) {}
+}
--- src/app/component/persion-select/persion-select.component.ts
+++ src/app/component/persion-select/persion-select.component.ts
@@ -1,19 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, forwardRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { ReactiveFormsModule, ControlValueAccessor } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  ControlValueAccessor,
+  NG_VALUE_ACCESSOR,
+} from '@angular/forms';
 import { NzInputModule } from 'ng-zorro-antd/input';
 
 @Component({
   selector: 'app-persion-select',
   standalone: true,
-  imports: [NzInputModule, ReactiveFormsModule, CommonModule],
+  imports: [NzInputModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './persion-select.component.html',
   styleUrl: './persion-select.component.less',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PersionSelectComponent),
+      multi: true,
+    },
+  ],
 })
 export class PersionSelectComponent implements ControlValueAccessor {
-  writeValue(value: any): void {}
-  registerOnChange(fn: (_: any) => void): void {}
-  registerOnTouched(fn: any): void {}
+  _personValue: string | null = '';
+  private _onChange = (_: any) => {};
+  private _onTouched = (_: any) => {};
+
+  modelChange(e: any) {
+    this.personValue = e;
+  }
+
+  set personValue(value) {
+    this._personValue = value;
+    this._onChange(this._personValue);
+  }
+
+  get personValue() {
+    return this._personValue;
+  }
+
+  writeValue(value: any): void {
+    if (value !== undefined) this.personValue = value;
+  }
+  registerOnChange(fn: (_: any) => void): void {
+    this._onChange = fn;
+  }
+  registerOnTouched(fn: any): void {
+    this._onTouched = fn;
+  }
   setDisabledState(isDisabled: boolean): void {}
 }
